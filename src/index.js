e64e90629b324d61833351a1f0f1fdc9449c6198
Zoe Weinstein <mxzweinstein@gmail.com>
2022-01-09 14:59:59 -0700

    updated some formatting

--- src/index.js
+++ src/index.js
@@ -20,10 +20,63 @@ $(document).ready(function(){
     });
     promise.then(function(response){
       const body = JSON.parse(response);
-      $("#picDisplay").html(`<img src=${body.urls.regular} alt=${body.alt_description}>`);
-      $("#attribution").html(`<a href=${body.user.links.html}>${body.user.name}</a>`);
+      $("#picDisplay").html(`<img src=${body.urls.small} alt=${body.alt_description}>`);
+      $("#photographer").html(`<a href=${body.user.links.html}>${body.user.name}</a>`);
+      $("#attribution").show();
     }, function(error){
       $('#showErrors').text(`There was an error processing your request: ${error}`)
     });
   });
+  $("#seachForm").submit(function(event){
+    event.preventDefault();
+    let searchTerm = $("#search").val();
+    let promise = new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      const url = `https://api.unsplash.com/search/photos/?query=${searchTerm}&client_id=${process.env.API_KEY}`
+      request.onload = function(){ 
+        if(this.status === 200){
+          resolve(request.response);
+        } else{
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+    promise.then(function(response){
+      const body = JSON.parse(response);
+      $("#picDisplay").html(`<img src=${body.results.urls.small} alt=${body.results.alt_description}>`);
+      $("#photographer").html(`<a href=${body.user.links.html}>${body.user.name}</a>`);
+      $("#attribution").show();
+    }, function(error){
+      $('#showErrors').text(`There was an error processing your request: ${error}`)
+    });
+    
+  });
+
+  $("#peopleButton").click(function(){
+    let promise = new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      const url = `https://api.unsplash.com/search/photos/?query=person&client_id=${process.env.API_KEY}`
+      request.onload = function(){
+        if(this.status === 200){
+          resolve(request.response);
+        } else{
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+    promise.then(function(response){
+      const body = JSON.parse(response);
+      $("#picDisplay").html(`<img src=${body.results.urls.small} alt=${body.results.alt_description}>`);
+      $("#photographer").html(`<a href=${body.user.links.html}>${body.user.name}</a>`);
+      $("#attribution").show();
+    }, function(error){
+      $('#showErrors').text(`There was an error processing your request: ${error}`)
+    });
+  })
+
+
 });
