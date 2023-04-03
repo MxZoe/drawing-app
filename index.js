53e5d82bc58669a92dc73ebe314df55bb21f71c0
Zoe Weinstein <mxzweinstein@gmail.com>
2023-04-03 08:25:21 -0700

    move file

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import $ from "jquery";
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import UnsplashService from './services/unsplash-service.js';
+import UnsplashService from './src/services/unsplash-service.js';
 
 
 function displayPic(response, color){
